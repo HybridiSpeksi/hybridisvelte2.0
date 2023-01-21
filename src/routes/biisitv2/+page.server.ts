b5e63678a4575596c9cc7059af731823933ffc8c
osanlan <osanlan@utu.fi>
2023-01-21 15:48:44 +0200

    new song tester

---
+++ src/routes/biisitv2/+page.server.ts
@@ -0,0 +1,38 @@
+import { error } from '@sveltejs/kit'
+import type { PageServerLoad } from "./$types";
+import contentfulFetch from '$lib/contentful-fetch';
+
+
+export const load: PageServerLoad = async () => {
+    const query = `
+    {
+        produktioCollection {
+            items {
+                biisitCollection {
+                    items {
+                        ...on Biisi {
+                            nimi
+                            vuosi
+                            origin
+                            sanat {
+                                json
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    `
+    const response = await contentfulFetch(query);
+    if (!response.ok) {
+        throw error(404, {
+            message: response.statusText
+        })
+    }
+
+    const { data } = await response.json();
+    return {
+        produktioData: data.produktioCollection
+    };
+}
