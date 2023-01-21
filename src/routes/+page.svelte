2b94938cec9f84370fba1fd088460adbe1b23b36
osanlan <36444262+osanlan@users.noreply.github.com>
2023-01-21 20:26:03 +0200

    header clickable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -135,7 +135,7 @@
         width: 100%;
         height: 100%;
         z-index: 1;
-        top: 0;
+        top: 50px;
         .left {
             position: relative;
             display: flex;
